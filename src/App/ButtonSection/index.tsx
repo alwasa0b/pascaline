80ef49d1a17c1aff29ec0621694997e3093dd142
alwasa0b <alalwan@gmail.com>
2022-02-07 09:21:17 -0500

    fix grid layout

--- src/App/ButtonSection/index.tsx
+++ src/App/ButtonSection/index.tsx
@@ -14,68 +14,61 @@ function ButtonSection() {
 
   return (
     <Grid container spacing={2} sx={{ p: 0, m: 0 }}>
-      <Grid item xs={6}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
+      <Grid container item xs={8} spacing={2}>
+        <Grid item xs={12}>
+          <Button
+            disabled={processing}
+            variant="contained"
+            size="large"
+            sx={{ borderRadius: 3 }}
+            fullWidth
+            onClick={() => updateValue("")}
+          >
+            AC
+          </Button>
+        </Grid>
+        {numbers.map((b) => (
+          <Grid key={`${b}`} item xs={4}>
             <Button
               disabled={processing}
-              variant="contained"
-              size="large"
-              sx={{ borderRadius: 3 }}
               fullWidth
-              onClick={() => updateValue("")}
-            >
-              AC
-            </Button>
-          </Grid>
-          {numbers.map((b) => (
-            <Grid key={`${b}`} item xs={4}>
-              <Button
-                disabled={processing}
-                fullWidth
-                variant="outlined"
-                size="large"
-                sx={{ borderRadius: 3 }}
-                onClick={() => appendValue(`${b}`)}
-              >
-                {b}
-              </Button>
-            </Grid>
-          ))}
-          <Grid item xs={8}>
-            <Button
-              disabled={processing}
               variant="outlined"
               size="large"
               sx={{ borderRadius: 3 }}
-              fullWidth
-              onClick={() => backspace()}
+              onClick={() => appendValue(`${b}`)}
             >
-              {`⌫`}
+              {b}
             </Button>
           </Grid>
+        ))}
+        <Grid item xs={8}>
+          <Button
+            disabled={processing}
+            variant="outlined"
+            size="large"
+            sx={{ borderRadius: 3 }}
+            fullWidth
+            onClick={() => backspace()}
+          >
+            {`⌫`}
+          </Button>
         </Grid>
       </Grid>
-      <Grid item xs={1}>
-        <Grid container spacing={2}></Grid>
-      </Grid>
-      <Grid item xs={3}>
-        <Grid container spacing={2}>
-          {operators.map((b) => (
-            <Grid key={`${b}`} item xs={12}>
-              <Button
-                disabled={processing}
-                fullWidth
-                variant="contained"
-                size="large"
-                sx={{ borderRadius: 3 }}
-                onClick={() => infixOperatorClicked(b)}
-              >
-                {b}
-              </Button>
-            </Grid>
-          ))}
-        </Grid>
+      <Grid container item xs={4} spacing={2}>
+        {operators.map((b) => (
+          <Grid key={`${b}`} item xs={12}>
+            <Button
+              disabled={processing}
+              fullWidth
+              variant="contained"
+              size="large"
+              sx={{ borderRadius: 3 }}
+              onClick={() => infixOperatorClicked(b)}
+            >
+              {b}
+            </Button>
+          </Grid>
+        ))}
       </Grid>
     </Grid>
   );
