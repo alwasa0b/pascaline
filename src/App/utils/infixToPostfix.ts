0ea0625f1da33c8e488e91d038107bd052abac59
alwasa0b <alalwan@gmail.com>
2022-02-07 20:48:03 -0500

    fix issue with negative numbers

--- src/App/utils/infixToPostfix.ts
+++ src/App/utils/infixToPostfix.ts
@@ -9,13 +9,17 @@ function precedent(c: string): 3 | 2 | 1 | -1 {
 export function infixToPostfix(infix: string | any[]) {
   const st = [];
   let result = "";
-
+  let lastParsed = "operator";
+  
   for (const char of infix) {
-    if (isNumeric(char)) {
+    if (isNumeric(char) || lastParsed === "operator") {
       result += char;
+      lastParsed = "number"
       continue;
     }
     // operator is scanned
+
+    lastParsed = "operator";
 
     while (st.length !== 0 && precedent(char) <= precedent(st[st.length - 1])) {
       result += ` ${st.pop()}`;
